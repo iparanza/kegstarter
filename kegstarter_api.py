bf7b9b1b73f342ea02fd1ec3dd84fddce06bf2e7
Jarrod Reuter <jarrod.reuter@gmail.com>
2014-05-11 11:36:49 -0400

    This should get a basic dev environment up.
    This displays a 'Hello World' message at <url>:8080/api.

---
+++ kegstarter_api.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/api')
+def hello():
+    return "Hello, World!"
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
